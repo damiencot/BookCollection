d669cb3907aba095ec2512d43de3f6e8a0f09a64
damiencot <damien.cot@gmail.com>
2021-01-26 11:23:44 +0100

    RecyclerView Home finish

--- app/src/main/java/fr/nansty/bookcollection/adapter/BookAdapter.kt
+++ app/src/main/java/fr/nansty/bookcollection/adapter/BookAdapter.kt
@@ -1,4 +1,30 @@
 package fr.nansty.bookcollection.adapter
 
-class BookAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import fr.nansty.bookcollection.R
+
+class BookAdapter(private val layoutId: Int) : RecyclerView.Adapter<BookAdapter.ViewHolder>(){
+
+    //boite pour ranger tout les composants à controler
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val bookImage = view.findViewById<ImageView>(R.id.image_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+                .from(parent.context)
+                .inflate(layoutId, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
+
+    override fun getItemCount(): Int {
+        return 5
+    }
 }
--- app/src/main/java/fr/nansty/bookcollection/fragments/HomeFragment.kt
+++ app/src/main/java/fr/nansty/bookcollection/fragments/HomeFragment.kt
@@ -5,11 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import fr.nansty.bookcollection.R
+import fr.nansty.bookcollection.adapter.BookAdapter
+import fr.nansty.bookcollection.adapter.BookItemDecoration
 
 class HomeFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        //recuperer le recyclerview
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recyclerView)
+        horizontalRecyclerView.adapter = BookAdapter(R.layout.item_horizontal_book)
+
+        //recupere le second recyclerView
+        val verticalRecyclerView = view.findViewById<RecyclerView>(R.id.vertical_recyclerView)
+        verticalRecyclerView.adapter = BookAdapter(R.layout.item_vertical_book)
+        verticalRecyclerView.addItemDecoration(BookItemDecoration( ))
+
+        return view
     }
 }
